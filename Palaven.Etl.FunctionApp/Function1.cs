2f19dd4e74eeaf8e84c8dcaeaf9b19f364a368b4
erickbr15 <erick_br15@hotmail.com>
2024-10-10 17:19:20 -0600

    palaven-llm. Added function to generate the bronze document.

---
+++ Palaven.Etl.FunctionApp/Function1.cs
@@ -0,0 +1,24 @@
+using Liara.Azure;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace Palaven.Etl.FunctionApp
+{
+    public class Function1
+    {
+        private readonly ILogger<Function1> _logger;
+
+        public Function1(ILogger<Function1> logger)
+        {
+            _logger = logger;
+        }
+
+        [Function(nameof(Function1))]
+        public async Task Run([BlobTrigger("etl-inbox/{name}", Connection = AzureClientConnectionStrings.BlobStorage)] Stream stream, string name)
+        {
+            using var blobStreamReader = new StreamReader(stream);
+            var content = await blobStreamReader.ReadToEndAsync();
+            _logger.LogInformation($"C# Blob trigger function Processed blob\n Name: {name} \n Data: {content}");
+        }
+    }
+}
